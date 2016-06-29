0925c9c29f8ba58c6bf9808511b5507ec555d725
jyotish09 <dontsentmemails@yahoo.com>
2016-06-29 20:01:44 +0530

    Trying to Read

--- app.js
+++ app.js
@@ -1,12 +1,62 @@
+
 	
 	// Angular ToDo
 	
 	var app = angular.module('myApp', []);
-	app.controller('todo', function($scope) {
-		$scope.todoList = [{todoText:'Clean House', done:false}];
+	app.controller('todoCtrl', function($scope) {
+
+		// Initialize Firebase
+	  
+		var config = {
+			apiKey: "",
+			authDomain: "",
+			databaseURL: "",
+			storageBucket: "",
+		};
+		
+		firebase.initializeApp(config);
+		
+		//console.log(firebase);
+		var ToDo = firebase.database().ref('ToDo');
+		//var Sample = firebase.database().ref('Sample');
+		
+		
+		
+		// for(i in $scope.todoList){
+		// 	console.log($scope.todoList[i]);
+		// 	ToDo.push($scope.todoList[i]);
+		// 	Sample.push($scope.todoList[i]);
+		// }
+		//$scope.todoList = [];
+
+		
+		// ToDo.on('value',function(todoResults){
+		// 	var todos = todoResults.val();
+		// 	$scope.todoRes = [];
+			
+		// 	//console.log("Reading from "+ToDo);
+		// 	for(j in todos){
+		// 		//console.log(todos[j]);
+		// 		$scope.todoRes.push(todos[j]);	
+		// 		//console.log($scope.todoRes);
+		// 	}
+
+		// 	setToDos($scope.todoRes);
+		// });
+
+		ToDo.once("value").then(function(snapshot){
+				$scope.todoList=snapshot.val();
+				console.log($scope.todoList);
+			});
+
+		function setToDos(x){
+			console.log(x);
+			$scope.todoList = x;
+			$scope.$apply();
+		}
 
 		$scope.todoAdd = function() {
-			$scope.todoList.push({todoText:$scope.todoInput, done:false});
+			ToDo.push({todoText:$scope.todoInput, done:false});
 			$scope.todoInput = "";
 		};
 
@@ -17,15 +67,5 @@
 				if (!x.done) $scope.todoList.push(x);
 			});
 		};
+		
 	});
-
-	  
-	// Initialize Firebase
-	  
-	  var config = {
-		apiKey: "",
-		authDomain: "",
-		databaseURL: "",
-		storageBucket: "",
-	  };
-	  firebase.initializeApp(config);
